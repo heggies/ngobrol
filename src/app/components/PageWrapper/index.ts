4ad5d4c559cae76a1034140e83328fce707ae216
Heggies Iqbal Wadie <heggies0iqbal@gmail.com>
2021-08-07 01:00:32 +0700

    refactor: PageWrapper width

--- src/app/components/PageWrapper/index.ts
+++ src/app/components/PageWrapper/index.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/macro'
 
 export const PageWrapper = styled.div`
-  width: 960px;
+  max-width: 960px;
+  width: 100%;
   height: 100%;
   margin: 0 auto;
   box-sizing: content-box;
