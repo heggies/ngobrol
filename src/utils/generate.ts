4b8a591d28973afcfd4fe26d391a3a7230596783
Heggies Iqbal Wadie <heggies0iqbal@gmail.com>
2021-08-06 22:29:48 +0700

    feat: random username generator

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -9,3 +9,58 @@ export function RandomString(length: number) {
 
   return result
 }
+
+export function RandomUsername() {
+  let sizes = [
+    'whopping',
+    'slim',
+    'chunky',
+    'gargantuan',
+    'hefty',
+    'teensy',
+    'strapping',
+    'slender',
+    'beefy',
+    'bulky',
+    'immense',
+    'massive',
+    'narrow',
+  ]
+
+  let animals = [
+    'alligator',
+    'baboon',
+    'coyote',
+    'deer',
+    'eagle',
+    'falcon',
+    'gecko',
+    'hamster',
+    'iguana',
+    'jellyfish',
+    'leopard',
+    'meerkat',
+    'newt',
+    'pug',
+    'quokka',
+    'rabbit',
+    'seal',
+    'tuna',
+    'uakari',
+    'weasel',
+    'xerus',
+    'yak',
+    'zonkey',
+  ]
+
+  let selectedSize = Math.floor(Math.random() * sizes.length)
+  let selectedAnimal = Math.floor(Math.random() * animals.length)
+
+  return (
+    sizes[selectedSize][0].toUpperCase() +
+    sizes[selectedSize].slice(1) +
+    (animals[selectedAnimal][0].toUpperCase() +
+      animals[selectedAnimal].slice(1)) +
+    Math.floor(Math.random() * 99)
+  )
+}
